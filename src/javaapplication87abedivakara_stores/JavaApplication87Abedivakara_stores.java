877f3777fdc5947c8176df77adcb00d9a22091eb
prasanjitha <graphicdis126@gmail.com>
2021-08-22 11:02:47 +0530

    created admin login page

---
+++ src/javaapplication87abedivakara_stores/JavaApplication87Abedivakara_stores.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication87abedivakara_stores;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication87Abedivakara_stores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
