2b8f32a079ecf06829197d95c0753a53409bbc5c
prasanjitha <graphicdis126@gmail.com>
2021-08-23 19:07:36 +0530

    add admin interface

--- src/javaapplication87abedivakara_stores/JavaApplication87Abedivakara_stores.java
+++ src/javaapplication87abedivakara_stores/JavaApplication87Abedivakara_stores.java
@@ -14,8 +14,34 @@ public class JavaApplication87Abedivakara_stores {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+  
+    
+}
+
+interface pi01{
+    default void m1(){
+        System.out.println("pi01");
+    }
+}
+interface pi02{
+    default void m1(){
+        System.out.println("pi02");
     }
     
+}
+class chi implements pi01,pi02{
+  
+      public static void main(String[] args) {
+        // TODO code application logic here
+          chi c=new chi();
+          c.m1();
+          c.m3();
+    }
+     public void m1(){
+          pi01.super.m1();
+          pi02.super.m1();
+      }
+      public void m3(){
+        System.out.println("hi");
+    }
 }
